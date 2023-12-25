ac7dd835b6acc48664bafe4f5546f6df5ab8fcae
gokusher <129699769+gokusher@users.noreply.github.com>
2023-12-26 00:27:38 +0600

    Add files via upload

--- sherbolot_37-1_hm6.py
+++ sherbolot_37-1_hm6.py
@@ -1,27 +1,57 @@
-def binary_search(element, sorted_list):
-    low = 0
-    high = len(sorted_list) - 1
+from random import randint
 
-    while low <= high:
-        mid = (low + high) // 2
-        guess = sorted_list[mid]
+def bubble_sort(num):
+    n = len(num)
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if num[j] > num[j + 1]:
+                num[j], num[j + 1] = num[j + 1], num[j]
+    return num
 
-        if guess == element:
-            return mid
-        elif guess < element:
-            low = mid + 1
+def selection_sort(num):
+    n = len(num)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if num[j] < num[min_index]:
+                min_index = j
+        num[i], num[min_index] = num[min_index], num[i]
+    return num
+
+def binary_search(find_num, num):
+    p = 0
+    f, l = 0, len(num) - 1
+    resultok = False
+    while f <= l:
+        m = (f + l) // 2
+        if num[m] == find_num:
+            f = m
+            l = f - 1
+            p = m
+            resultok = True
+        elif num[m] < find_num:
+            f = m + 1
         else:
-            high = mid - 1
+            l = m - 1
+    if resultok:
+        print(f'{find_num} is found! Index: {p}')
+    else:
+        print(f'{find_num} is not found')
 
-    return -1
+numbs = []
+for i in range(1, 11):
+    numbs.append(randint(10, 50))
 
-
-moi_sorted_list = [11, 12, 22, 25, 34, 64, 90]
-element_to_find = 22
-
-result = binary_search(element_to_find, moi_sorted_list)
-
-if result != -1:
-    print(f"Элемент {element_to_find} найден по индексу {result}.")
+menu = input('Bubble sort or selection sort? ')
+if menu == 'bubble':
+    sorted_list = bubble_sort(numbs)
+elif menu == 'selection':
+    sorted_list = selection_sort(numbs)
 else:
-    print(f"Элемент {element_to_find} не найден в списке.")
+    print('Command is not found')
+
+if menu == 'bubble' or menu == 'selection':
+    print("Sorted List:", sorted_list)
+    search = 28
+    binary_search(search, numbs)
+
